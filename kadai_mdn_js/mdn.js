d1b0d0f7572dda3943aae8442c53c985899ec82f
laiyajp-debug <laiyajp@gmail.com>
2025-12-15 21:02:37 +0900

    jsを差し替え

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,6 +1,6 @@
 const now = new Date();
 
 const year = now.getFullYear();
-const month = now.getMonth();
+const month = now.getMonth() + 1;
 const day = now.getDate();
 console.log(`${year}年${month}月${day}日`);
